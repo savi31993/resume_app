24ea4931005a56a1279bc2b9d6eb215f1defecfd
Savithri K B <savi31993@gmail.com>
2019-09-16 14:39:53 -0400

    modified  layout

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def template
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get '/template' => 'home#template'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
